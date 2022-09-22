4b7da6baee44c6a7419618fa20ac2839e21a91ab
cpshibinoppo <cpshibinoppo@gmail.com>
2022-09-22 11:41:10 +0530

    add sider bar

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  showsubnavbar() {
+    const sub = document.getElementById('subnav');
+    const navbar = document.getElementById('navbar');
+    navbar!.style.display = 'none';
+    sub!.style.display = 'block';
   }
-
 }
