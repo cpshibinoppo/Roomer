ae6713a5e1fd8d637be7e421d9591ba47ea5cc4a
cpshibinoppo <cpshibinoppo@gmail.com>
2022-09-30 17:26:33 +0530

    ng bulid

--- src/app/layout/subnavbar/subnavbar.component.ts
+++ src/app/layout/subnavbar/subnavbar.component.ts
@@ -1,11 +1,10 @@
 import { Component,ViewEncapsulation, OnInit, ViewChild } from '@angular/core';
 import filterdata from './filterdetails.json';
 import { SwiperComponent } from "swiper/angular";
-// import SwiperCore, { Navigation } from "swiper";
-// import Swiper from 'swiper';
-import SwiperCore, { Navigation, Swiper, Virtual,Pagination } from 'swiper';
 
-SwiperCore.use([Navigation,Pagination]);
+import SwiperCore, { Navigation } from 'swiper';
+
+SwiperCore.use([Navigation]);
 
 interface filterData {
   name: String;
@@ -22,17 +21,12 @@ export class SubnavbarComponent implements OnInit {
   filterData: filterData[] = filterdata;
   constructor() {}
   @ViewChild('swiper', { static: false }) swiper?: SwiperComponent;
-  slideNext(){
-    this.swiper!.swiperRef.slideNext(100);
-  }
-  slidePrev(){
-    this.swiper!.swiperRef.slidePrev(100);
-  }
   ngOnInit(): void {}
   swiperConfig: any = {
-    slidesPerView: 20,
+    slidesPerView: 'auto',
     navigation: true,
     spaceBetween: 20,
+    // loop: true,
     slidesPerGroup:10,
 }
 
