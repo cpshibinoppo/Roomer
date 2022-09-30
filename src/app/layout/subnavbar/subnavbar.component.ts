f659cf21dca5532d07a540742ca32211bb9c104e
cpshibinoppo <cpshibinoppo@gmail.com>
2022-09-30 10:12:13 +0530

    add subnavbar folder

---
+++ src/app/layout/subnavbar/subnavbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subnavbar',
+  templateUrl: './subnavbar.component.html',
+  styleUrls: ['./subnavbar.component.scss']
+})
+export class SubnavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
