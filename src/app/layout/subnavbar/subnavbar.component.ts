e4e03a0cd2425253ac9c2d8608d9ac05d61cbd58
cpshibinoppo <cpshibinoppo@gmail.com>
2022-09-30 15:49:39 +0530

    add subnavbarslider

--- src/app/layout/subnavbar/subnavbar.component.ts
+++ src/app/layout/subnavbar/subnavbar.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ViewEncapsulation, OnInit, ViewChild } from '@angular/core';
+import filterdata from './filterdetails.json';
+import { SwiperComponent } from "swiper/angular";
+// import SwiperCore, { Navigation } from "swiper";
+// import Swiper from 'swiper';
+import SwiperCore, { Navigation, Swiper, Virtual,Pagination } from 'swiper';
+
+SwiperCore.use([Navigation,Pagination]);
+
+interface filterData {
+  name: String;
+  img: String;
+}
 
 @Component({
   selector: 'app-subnavbar',
   templateUrl: './subnavbar.component.html',
-  styleUrls: ['./subnavbar.component.scss']
+  styleUrls: ['./subnavbar.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class SubnavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  filterData: filterData[] = filterdata;
+  constructor() {}
+  @ViewChild('swiper', { static: false }) swiper?: SwiperComponent;
+  slideNext(){
+    this.swiper!.swiperRef.slideNext(100);
   }
+  slidePrev(){
+    this.swiper!.swiperRef.slidePrev(100);
+  }
+  ngOnInit(): void {}
+  swiperConfig: any = {
+    slidesPerView: 20,
+    navigation: true,
+    spaceBetween: 20,
+    slidesPerGroup:10,
+}
 
 }
