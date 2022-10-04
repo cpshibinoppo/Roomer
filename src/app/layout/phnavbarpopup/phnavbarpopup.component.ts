8e833cfe97e066eb37ff86eb0ffadad025552ab2
cpshibinoppo <cpshibinoppo@gmail.com>
2022-10-04 16:12:06 +0530

    add header in phnavbar

--- src/app/layout/phnavbarpopup/phnavbarpopup.component.ts
+++ src/app/layout/phnavbarpopup/phnavbarpopup.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import $ from 'jquery';
 @Component({
   selector: 'app-phnavbarpopup',
   templateUrl: './phnavbarpopup.component.html',
-  styleUrls: ['./phnavbarpopup.component.scss']
+  styleUrls: ['./phnavbarpopup.component.scss'],
 })
 export class PhnavbarpopupComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    $(document).ready(function () {
+      $('.headerbtnmaindiv button').click(function () {
+        var check = $(this).text();
+        if (check == 'Experiences') {
+          $('.headerline').css({ left: '70px', width: '90px' });
+        } else {
+          $('.headerline').css({ left: '10px', width: '40px' });
+        }
+        $('.headerbtnmaindiv button').removeClass('headerbutactive');
+        $(this).addClass('headerbutactive');
+      });
+    });
   }
 
+  ngOnInit(): void {}
 }
