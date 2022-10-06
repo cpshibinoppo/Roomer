428fd043e4fcabe54c80be669e52cede8e97e256
cpshibinoppo <cpshibinoppo@gmail.com>
2022-10-06 10:15:16 +0530

    phnavbar allmostcomplated

--- src/app/layout/phnavbarpopup/phnavbarpopup.component.ts
+++ src/app/layout/phnavbarpopup/phnavbarpopup.component.ts
@@ -27,18 +27,23 @@ export class PhnavbarpopupComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
   addadults() {
     if (this.guests.adults == 0) {
       this.guests.adults = 1;
     }
   }
-  subshow(check:any){
-if (check == 'where'){
-  $('.whereshowdiv').css('display', 'block');
-  $('.gust').css('display', 'none');
-
-
-}
+  subshow(check: any) {
+    if (check == 'where') {
+      $('.whereshowdiv').css('display', 'block');
+      $('.where').css('display', 'none');
+      $('.whosection').css('display', 'none');
+      $('.guest').css('display', 'block');
+    }else if (check == 'guest'){
+      $('.whosection').css('display', 'block');
+      $('.guest').css('display', 'none');
+      $('.where').css('display', 'block');
+      $('.whereshowdiv').css('display', 'none');
+    }
   }
 }
