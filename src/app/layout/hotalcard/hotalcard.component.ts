9b79ee91e21ecaaaa5d28435de1a0a50b9c9b40e
cpshibinoppo <cpshibinoppo@gmail.com>
2022-10-15 10:27:04 +0530

    hotal card desing all most complated

--- src/app/layout/hotalcard/hotalcard.component.ts
+++ src/app/layout/hotalcard/hotalcard.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 
 import $ from 'jquery';
 import { SwiperComponent } from 'swiper/angular';
+import SwiperCore, { Navigation,Pagination } from "swiper";
 
-
-
+SwiperCore.use([Navigation,Pagination]);
 @Component({
   selector: 'app-hotalcard',
   templateUrl: './hotalcard.component.html',
   styleUrls: ['./hotalcard.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class HotalcardComponent implements OnInit {
 
@@ -18,13 +18,14 @@ export class HotalcardComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  @ViewChild(SwiperComponent) swiper: SwiperComponent | undefined;
+  // @ViewChild(SwiperComponent) swiper: SwiperComponent | undefined;
+  // @ViewChild(SwiperDirective) swiperDirectiveRef: SwiperDirective;
 
 
-  swipePrev() {
-    this.swiper!.swiperRef.slidePrev();
-  }
-  swipeNext() {
-    this.swiper!.swiperRef.slideNext();
-  }
+  // swipePrev() {
+  //   this.swiper!.swiperRef.slidePrev();
+  // }
+  // swipeNext() {
+  //   this.swiper!.swiperRef.slideNext();
+  // }
 }
