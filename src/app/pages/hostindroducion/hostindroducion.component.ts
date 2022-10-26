60392121c018fcfb1a72daf08f4bd333c9f1b92a
cpshibinoppo <cpshibinoppo@gmail.com>
2022-10-26 06:13:38 +0530

    exit button add

--- src/app/pages/hostindroducion/hostindroducion.component.ts
+++ src/app/pages/hostindroducion/hostindroducion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-hostindroducion',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hostindroducion.component.scss']
 })
 export class HostindroducionComponent implements OnInit {
-
+  playicon = true;
+  @ViewChild("videoPlayer", { static: false }) videoplayer: ElementRef | undefined;
   constructor() { }
 
   ngOnInit(): void {
+    this.playicon = true;
   }
+  playPause() {
+    var myVideo: any = document.getElementById("my_video_1");
+    if (myVideo.paused) {
+      myVideo.play();
+      this.playicon = false;
+      console.log(myVideo);
 
+    }
+    else if (myVideo.play) {
+      myVideo.pause();
+      this.playicon = true;
+    }
+  }
 }
