1097279e710d4270b4608865e0fde3e1b5080896
cpshibinoppo <cpshibinoppo@gmail.com>
2022-10-31 13:52:42 +0530

    gi tpush

---
+++ src/app/shared/form.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormService {
+  private subject = new Subject<any>();
+  constructor() { }
+  sendClickEvent() {
+    this.subject.next(null);
+  }
+  getClickEvent(): Observable<any>{
+    return this.subject.asObservable();
+  }
+}
