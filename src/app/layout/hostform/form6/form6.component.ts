78447eb2fd75040a1d2baedab8f3d938bdfcf6ed
cpshibinoppo <cpshibinoppo@gmail.com>
2022-11-03 18:25:30 +0530

    add siome cont in form 6

--- src/app/layout/hostform/form6/form6.component.ts
+++ src/app/layout/hostform/form6/form6.component.ts
@@ -6,13 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form6.component.scss']
 })
 export class Form6Component implements OnInit {
- dur = 3.3333333333333335;
+
   constructor() { }
 
   ngOnInit(): void {
-    $( "#trigger" ).click(function() {
-      // document.getElementById("ani-circle").beginElement();
-    });
+    $('.boxmandiv button').click(function () {
+      $('.active').removeClass('active');
+      $(this).addClass('active');
+    })
   }
 
 }
