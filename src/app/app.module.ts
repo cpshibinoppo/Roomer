8d3279e54bab1b8a6b7a5498639481fdae165282
cpshibinoppo <cpshibinoppo@gmail.com>
2022-12-17 14:17:01 +0530

    code clear

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -92,6 +93,7 @@ import { ShowmoreimgComponent } from './layout/showmoreimg/showmoreimg.component
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    HttpClientModule,
     SwiperModule
   ],
   providers: [],
